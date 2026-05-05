58c06aec225650a703874f8ad0c519c320371a34
Imran shiundu <143967151+imranshiundu@users.noreply.github.com>
2026-05-06 00:40:48 +0300

    chore: add Hoovi resolver barrel export

---
+++ lib/hoovi/resolver/resolver.dart
@@ -0,0 +1,7 @@
+export 'hoovi_audio_candidate.dart';
+export 'hoovi_match_result.dart';
+export 'hoovi_match_scorer.dart';
+export 'hoovi_resolver.dart';
+export 'hoovi_source_adapter.dart';
+export 'hoovi_track_identity.dart';
+export 'adapters/hoovi_local_file_source_adapter.dart';
